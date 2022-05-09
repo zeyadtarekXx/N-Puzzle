603dfa649cb7546c47a80149f56fc89254d0aff2
saranaem <63459509+saranaem@users.noreply.github.com>
2022-05-09 16:21:32 +0200

    adding hamming and manhattan

--- Helpers.cs
+++ Helpers.cs
@@ -1,50 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace NPuzzle
 {
     class Helpers
     {
-        public int ManhattanDistance(int[,] current)
+        public int ManhattanDistance(int[] current)
         {
             int n = (int)Math.Sqrt(current.Length);
             int md = 0;
-            for (int i = 0; i < n; i++)
+            int size = n * n;
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < n; j++)
+                if (current[i] != 0 && current[i] != i + 1)
                 {
-                    if (current[i, j] != 0 && current[i, j] != i * n + j + 1)//only enter if the number isn't in order
-                    {
-                        //look for this element in goal to compare 
-                        /* for (int k = 0; k < n; k++)
-                             for (int h = 0; h < n; h++)
-                                 if (current[i, j] == goal[k, h])
-                                     md += Math.Abs(i - k) + Math.Abs(j - h);
-                         */
-                        int x, y;
-                        x = Math.DivRem(current[i, j] - 1, n,out y);
-                        md += Math.Abs(i - x) + Math.Abs(j - y);
-                    }
+                    int x, y;
+                    x = Math.DivRem(current[i] - 1, n, out y);
+                    md += Math.Abs(i / 3 - x) + Math.Abs(i % 3 - y);
                 }
             }
             return md;
         }
 
-        public int HammingDistance(int[,] current)
+        public int HammingDistance(int[] current)
         {
-            int n = (int)Math.Sqrt(current.Length);
+            int size= current.Length;
             int hd = 0;
-            for (int i = 0; i < n; i++)
+            for (int i = 0; i < size; i++)
             {
-                for (int j = 0; j < n; j++)
+                if (current[i] != 0 && current[i] != i+1)
                 {
-                    if (current[i, j] != 0 && current[i, j] != i*n+j+1)
-                    {
-                        hd++;
-                    }
+                    hd++;
                 }
-            } 
+            }
            return hd;
         }
     }
