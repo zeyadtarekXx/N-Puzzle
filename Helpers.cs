ac3cdccfc24d98152bb14bb3657af060befb759b
saranaem <63459509+saranaem@users.noreply.github.com>
2022-05-10 00:53:13 +0200

    Update Helpers.cs

--- Helpers.cs
+++ Helpers.cs
@@ -4,7 +4,7 @@ namespace NPuzzle
 {
     class Helpers
     {
-        public int ManhattanDistance(int[] current)
+        public static int ManhattanDistance(int[] current)
         {
             int n = (int)Math.Sqrt(current.Length);
             int md = 0;
@@ -21,7 +21,7 @@ namespace NPuzzle
             return md;
         }
 
-        public int HammingDistance(int[] current)
+        public static int HammingDistance(int[] current)
         {
             int size= current.Length;
             int hd = 0;
@@ -33,6 +33,71 @@ namespace NPuzzle
                 }
             }
            return hd;
+        }
+        public static Node moveRight(int indx, Node current)
+        {
+            Node newPuzzle = new Node(current.perimeter, current.puzzle);
+            newPuzzle.parent = current;
+
+            newPuzzle.puzzle[indx + 1] = current.puzzle[indx];
+            newPuzzle.puzzle[indx] = current.puzzle[indx + 1];
+
+            newPuzzle.puzzleToStr();
+            newPuzzle.zeroIndx += 1;
+
+            return newPuzzle;
+        }
+        public static Node moveLeft(int indx, Node current)
+        {
+            Node newPuzzle = new Node(current.perimeter,current.puzzle);
+            newPuzzle.parent = current;
+
+            newPuzzle.puzzle[indx-1] = current.puzzle[indx];
+            newPuzzle.puzzle[indx] = current.puzzle[indx - 1];
+
+            newPuzzle.puzzleToStr();
+            newPuzzle.zeroIndx -= 1;
+
+            return newPuzzle;
+        }
+        public static Node moveUp(int indx, Node current)
+        {
+            Node newPuzzle = new Node(current.perimeter, current.puzzle);
+            newPuzzle.parent = current;
+
+            newPuzzle.puzzle[indx - current.perimeter ] = current.puzzle[indx];
+            newPuzzle.puzzle[indx] = current.puzzle[indx - current.perimeter];
+
+            newPuzzle.puzzleToStr();
+            newPuzzle.zeroIndx -= current.perimeter;
+
+            return newPuzzle;
+
+        }
+        public static Node moveDown(int indx, Node current)
+        {
+            Node newPuzzle = new Node(current.perimeter, current.puzzle);
+            newPuzzle.parent = current;
+
+            newPuzzle.puzzle[indx + current.perimeter] = current.puzzle[indx];
+            newPuzzle.puzzle[indx] = current.puzzle[indx + current.perimeter];
+
+            newPuzzle.puzzleToStr();
+            newPuzzle.zeroIndx += current.perimeter;
+
+            return newPuzzle;
+        }
+        public static bool goal(int [] puzzle)
+        {
+            int size = puzzle.Length;
+            for (int i = 0; i < size-1; i++)
+            {
+                if (puzzle[i] != i + 1)
+                {
+                    return false; 
+                }
+            }
+            return true;
         }
     }
 }
