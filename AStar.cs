adf06ab9ff61347012cb02a720db330231d9797c
saranaem <63459509+saranaem@users.noreply.github.com>
2022-05-10 23:47:40 +0200

    modifications

--- AStar.cs
+++ AStar.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,43 +8,70 @@ namespace NPuzzle
     class AStar
     {
         private HashSet<string> hashset= new HashSet<string>();
-        private Queue<Node> pqueue;
+        private PriorityQueue pqueue;
 
         public void solver(Node parentnode)
         {
-            hashset.Add(parentnode.puzzleStr);
-            pqueue.Enqueue(parentnode);
-            Node node;
+            hashset.Add(new string(parentnode.puzzleStr));
+            Node node=null;
+            int count = 0;
             while(true)
             {
-                node = pqueue.Dequeue();
+                if (node==null)
+                {
+                    node = parentnode;
+                    Console.WriteLine(node.puzzleStr);
+                }
+                else
+                  node = pqueue.getMin();
+                //Console.WriteLine("\n");
+
                 if (Helpers.goal(node.puzzle))
                 {
-                   
+                    Console.WriteLine(count);
                     break;
                 }
                 else
                 {
                     int zeroindx= node.zeroIndx;
+                    if (count == 21)
+                        count++;
                     PotentialNodes(zeroindx, node);
 
+                    Console.WriteLine(pqueue.getMin().puzzleStr);
+                    
+
                 }
+                //for (int i = 0; i < node.childrenOfNode.Count; i++)
+                //{
+                    //Console.WriteLine("Childern");
+                    //Console.WriteLine(node.childrenOfNode[i].puzzleStr);
+                //}
+            
+                count++;
 
             }
         }
         public void PotentialNodes(int indx, Node current)
         {
+
+            pqueue = new PriorityQueue();
             int zeroRow= indx / current.perimeter;
             int zeroCol = indx % current.perimeter;
            
             if (zeroCol < current.perimeter - 1)
             {
-                Node newPuzzle = Helpers.moveRight(indx, current);
+                Node newPuzzle = new Node();
+                newPuzzle=Helpers.moveRight(indx, current);
+                //if(newPuzzle==null)
+                //Console.WriteLine("right");
+                // Console.WriteLine(newPuzzle.puzzleStr);
+                // Console.WriteLine(newPuzzle.puzzleStr);
                 if (!hashset.Contains(newPuzzle.puzzleStr))
                 {
                     newPuzzle.set_F(current.g + 1, Helpers.ManhattanDistance(newPuzzle.puzzle));
                     newPuzzle.expansion_order = current.expansion_order + 1;
-                    pqueue.Enqueue(newPuzzle);
+                    pqueue.HeapInsert(newPuzzle);
                     hashset.Add(newPuzzle.puzzleStr);
                     current.childrenOfNode.Add(newPuzzle);
 
@@ -53,12 +80,14 @@ namespace NPuzzle
             if (zeroCol > 0)
             {
                 Node newPuzzle = Helpers.moveLeft(indx, current);
+                //Console.WriteLine("left");
+                //Console.WriteLine(newPuzzle.puzzleStr);
                 if (!hashset.Contains(newPuzzle.puzzleStr))
                 {
                     newPuzzle.set_F(current.g + 1, Helpers.ManhattanDistance(newPuzzle.puzzle));
                     newPuzzle.expansion_order = current.expansion_order + 1;
-                    pqueue.Enqueue(newPuzzle);
-                    hashset.Add(newPuzzle.puzzleStr);
+                    pqueue.HeapInsert(newPuzzle);
+                    hashset.Add(new string(newPuzzle.puzzleStr));
                     current.childrenOfNode.Add(newPuzzle);
                 }
 
@@ -66,12 +95,14 @@ namespace NPuzzle
             if (zeroRow < current.perimeter - 1)
             {
                 Node newPuzzle = Helpers.moveDown(indx, current);
+                //Console.WriteLine("down");
+                //Console.WriteLine(newPuzzle.puzzleStr);
                 if (!hashset.Contains(newPuzzle.puzzleStr))
                 {
                     newPuzzle.set_F(current.g + 1, Helpers.ManhattanDistance(newPuzzle.puzzle));
                     newPuzzle.expansion_order = current.expansion_order + 1;
-                    pqueue.Enqueue(newPuzzle);
-                    hashset.Add(newPuzzle.puzzleStr);
+                    pqueue.HeapInsert(newPuzzle);
+                    hashset.Add(new string(newPuzzle.puzzleStr));
                     current.childrenOfNode.Add(newPuzzle);
                 }
 
@@ -79,12 +110,14 @@ namespace NPuzzle
             if (zeroRow > 0)
             {
                 Node newPuzzle = Helpers.moveUp(indx, current);
+                //Console.WriteLine("up");
+                //Console.WriteLine(newPuzzle.puzzleStr);
                 if (!hashset.Contains(newPuzzle.puzzleStr))
                 {
                     newPuzzle.set_F(current.g + 1, Helpers.ManhattanDistance(newPuzzle.puzzle));
                     newPuzzle.expansion_order = current.expansion_order + 1;
-                    pqueue.Enqueue(newPuzzle);
-                    hashset.Add(newPuzzle.puzzleStr);
+                    pqueue.HeapInsert(newPuzzle);
+                    hashset.Add(new string(newPuzzle.puzzleStr));
                     current.childrenOfNode.Add(newPuzzle);
                 }
 
