b61056b63f34e31a10871c0d7545b4515e30682f
marvelsamuel <76208265+marvelsamuel@users.noreply.github.com>
2022-05-17 17:23:52 +0200

    Update ReadfromFile.cs

--- ReadfromFile.cs
+++ ReadfromFile.cs
@@ -1,7 +1,10 @@
 using System;
 using System.IO;
 
-class Readfromfile
+
+namespace ConsoleApp3
+{
+    class Readfromfile
     {
         public int N;
         public int[] p;
@@ -13,15 +16,22 @@ class Readfromfile
             N = int.Parse(Console.ReadLine());
             p = new int[N * N];
             int k = 0;
-            Console.ReadLine();
-            for (int i = 0; i < N; i++)
+            //Console.ReadLine();
+            for (int i = 0; i < N ; i++)
             {
-                string[] lineItems = Console.ReadLine().Split(' ');
+                string lineItems = Console.ReadLine();
+                if (lineItems== String.Empty)
+                {
+                    i--;
+                    continue;
+                }
+                string[] lineItems2 = lineItems.Split(' ');
                 for (int j = 0; j < N; j++)
                 {
-                    p[k++] = int.Parse(lineItems[j]);
+                    p[k++] = int.Parse(lineItems2[j]);
                 }
             }
             sr.Close();
         }
     }
+}
